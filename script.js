2d85cbc25f50a1dcc4fb2b3e5f038fa56b46efda
sitesh221b <siteshroy786@gmail.com>
2018-12-31 18:52:58 +0530

    Fully functional

--- script.js
+++ script.js
@@ -7,12 +7,14 @@ let body = document.getElementById('tasks');
 //stores the array of close buttons
 var closeBtn = document.getElementsByClassName('remove');
 
+//each para of the List
+var para = document.getElementsByClassName('task');
 
 function addTask(){
     // Each div for each item
     let div = document.createElement('div');
     // div.setAttribute('class', 'alert alert-warning alert-dismissible');
-    div.setAttribute('class', 'alert alert-warning');
+    div.setAttribute('class', 'alert alert-warning div-style');
     let item = document.createElement('p');
     item.setAttribute('class', 'task');
     // content has the actual text
@@ -24,13 +26,12 @@ function addTask(){
     // close_button.setAttribute('aria-label', 'Close');
     let icon = document.createElement('span');
     // icon.setAttribute('aria-hidden', 'true');
-    icon.textContent = '×';
-
+    icon.textContent = "\u00D7";
+    // icon.appendChild(xIcon)
     if(txt.value === ''){
         alert("Please enter a task!");
         txt.focus();
-    }
-    else{
+    } else{
         // localStorage.setItem('list', txt.value)
         item.appendChild(content);
         close_button.appendChild(icon);
@@ -41,16 +42,35 @@ function addTask(){
     }
     document.getElementById('task-text').value = '';
 
-    for(var i=0; i<closeBtn.length; i++){
+    // To remove items
+    for(let i=0; i<closeBtn.length; i++){
         closeBtn[i].onclick = function(){
             this.parentElement.remove();
         }
     }
 
+    // To cross off the items
+    for(let i=0; i<para.length; i++){
+        para[i].onclick = function(){
+            if(this.style.textDecoration === "line-through")
+                this.style.textDecoration = "none";
+            else
+                this.style.textDecoration = "line-through"
+        }
+    }
 }
 
 // for(var i=0; i<closeBtn.length; i++){
 //     closeBtn[i].onclick = function(){
 //         this.parentElement.remove();
+//     }
+// }
+
+// for(let i=0; i<para.length; i++){
+//     para[i].onclick = function(){
+//         if(this.style.textDecoration === "line-through")
+//             this.style.textDecoration = "none";
+//         else
+//             this.style.textDecoration = "line-through"
 //     }
 // }
