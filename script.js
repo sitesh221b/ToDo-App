9d9b20beadef519667d98b4402bf03700af28a2e
sitesh221b <siteshroy786@gmail.com>
2018-12-31 16:47:40 +0530

    Close button working

--- script.js
+++ script.js
@@ -1,17 +1,26 @@
-let addTask = function(){
-    let txt = document.getElementById('task-text');
-    let body = document.getElementById('tasks');
+//have the text of to-do by the user
+var txt = document.getElementById('task-text');
+
+//the body of the list
+let body = document.getElementById('tasks');
+
+//stores the array of close buttons
+var closeBtn = document.getElementsByClassName('remove');
+
+
+function addTask(){
     // Each div for each item
     let div = document.createElement('div');
-    div.setAttribute('class', 'alert alert-warning alert-dismissible');
+    // div.setAttribute('class', 'alert alert-warning alert-dismissible');
+    div.setAttribute('class', 'alert alert-warning');
     let item = document.createElement('p');
     item.setAttribute('class', 'task');
     // content has the actual text
     let content = document.createTextNode(txt.value);
     let close_button = document.createElement('button');
-    close_button.setAttribute('class', 'close');
-    close_button.setAttribute('type', 'button');
-    close_button.setAttribute('data-dismiss', 'alert');
+    close_button.setAttribute('class', 'remove');
+    // close_button.setAttribute('type', 'button');
+    // close_button.setAttribute('data-dismiss', 'alert');
     // close_button.setAttribute('aria-label', 'Close');
     let icon = document.createElement('span');
     // icon.setAttribute('aria-hidden', 'true');
@@ -22,7 +31,7 @@ let addTask = function(){
         txt.focus();
     }
     else{
-        localStorage.setItem('list', txt.value)
+        // localStorage.setItem('list', txt.value)
         item.appendChild(content);
         close_button.appendChild(icon);
         div.appendChild(item);
@@ -31,7 +40,17 @@ let addTask = function(){
         txt.focus();
     }
     document.getElementById('task-text').value = '';
-    // close_button.addEventListener('onclick', function(){
-    //     localStorage.removeItem('list')
-    });
-};
+
+    for(var i=0; i<closeBtn.length; i++){
+        closeBtn[i].onclick = function(){
+            this.parentElement.remove();
+        }
+    }
+
+}
+
+// for(var i=0; i<closeBtn.length; i++){
+//     closeBtn[i].onclick = function(){
+//         this.parentElement.remove();
+//     }
+// }
